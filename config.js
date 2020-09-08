039f08c59b3cabbd123ac25c304a4ce59cea7145
addiebarron <addiebarronemail@gmail.com>
2020-09-07 21:16:34 -0400

    Updated gitignore 2

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	// currently set to @lengthypooch API creds
-	twitter: {
+	twitterKeys: {
 		consumer_key: 			'uDRv6L6q31WU9EGTvYLZWCQ7I',
 		consumer_secret: 		'nDG5tHIPdozvHC4iHp1xlWNjdoW0wWi3p1ytmJ3vyIUhBg2xSW',
 		access_token_key: 		'1729320229-0IVPIhiGl96sFJzaZcS9Dgliz0DWx3Lgc04RUuz',
--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import { readFile, writeFile } from 'fs/promises';
 
-import config from './config'
+import {
+	config,
+	ipfile,
+	handle
+} from './config'
 
 // run
 
@@ -9,21 +13,34 @@ import cp from 'child_process'
 const exec = util.promisify(cp.exec);
 
 export async function run () {
+	
+
+	let currentIP, savedIP;
+
 	try {
-		const currentIP = (await exec(`wget -q -O - checkip.dyndns.com`)).stdout
+		currentIP = ( await exec(`wget -q -O - checkip.dyndns.com`) )
+			.stdout
 			.replace(/.+(IP Address: )([\d\.]+).+/, '$2')
 			.trim();
 
-		const savedIP = (await readFile(config.savedIPLocation, 'utf-8')).trim();
+	} catch (err) {
+		console.log(err)
+	}
+
+	try {
+		savedIP = ( await readFile(ipfile, 'utf-8') ).trim()
 
 		if (savedIP !== currentIP) {
-			update(currentIP);
+			save(currentIP);
 			tweet(savedIP, currentIP);
 		} else {
 			console.log('IP Address has not changed.')
 		}
 	} catch (err) {
-		console.error(err);
+
+		await writeFile(ipfile, currentIP, 'utf-8');
+
+		console.log(`No saved IP found. Saved current IP (${currentIP}) to ${ipfile}`);
 	}
 }
 
@@ -33,7 +50,11 @@ import Twitter from 'twitter-lite'
 
 async function tweet (oldIP, newIP) {
 	const twitter = new Twitter(config.twitter);
-	const tweetText = `@${config.twitterHandle} our router's public ip address has changed from ${oldIP} to ${newIP}. time to update dns!!`;
+	const tweetText = `our router's public ip address has changed from ${oldIP} to:
+
+${newIP}
+
+time to update dns!`;
 
 	try {
 		await twitter.post('statuses/update', {
@@ -44,8 +65,8 @@ async function tweet (oldIP, newIP) {
 	}
 }
 
-// update saved IP to match current one, if necessary
+// replace saved IP with current one
 
-async function update (newIP) {
-	await writeFile(config.savedIPLocation, newIP, 'utf-8');
+async function save (newIP) {
+	await writeFile(ipfile, newIP, 'utf-8');
 }
