53a8f21613253585ea316c47c63bd9d304cd7ed7
addiebarron <addiebarronemail@gmail.com>
2020-09-07 23:19:25 -0400

    First commit - working on Mac

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
+const twitterKeys = require('./keys/twitter');
+
 module.exports = {
-	// currently set to @lengthypooch API creds
-	twitterKeys: {
-		consumer_key: 			'uDRv6L6q31WU9EGTvYLZWCQ7I',
-		consumer_secret: 		'nDG5tHIPdozvHC4iHp1xlWNjdoW0wWi3p1ytmJ3vyIUhBg2xSW',
-		access_token_key: 		'1729320229-0IVPIhiGl96sFJzaZcS9Dgliz0DWx3Lgc04RUuz',
-		access_token_secret: 	'fNUjfQ37RT2FgjiBeac3SAjcugMerBFokhVgT25OM7SVB',
-	},
+	// use your own twitter keys here!
+	twitterKeys,
 	savedIPLocation: 'savedip.txt',
-	twitterHandle: 'lengthypooch'
+	twitterHandle: 'lengthypooch',
 }
